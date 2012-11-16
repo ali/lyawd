ea2f4fa975900a874e4816c6a5188af0ba8ec933
Ali Ukani <ali.ukani@gmail.com>
2012-11-16 00:52:01 -0500

    Created a more interesting 'hello world' app

--- day_two/ruby/sinatra.rb
+++ day_two/ruby/sinatra.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
 
+# Sinatra is a really cool, lightweight Ruby
+# framework for making websites.
+# You can learn more at http://sinatrarb.com
+
+# A GET request to the root of the domain ('/')
 get '/' do
-  'Hello, world!'
+  # Create an array of nouns and an array of adjectives
+  nouns = ['Crew',
+    'Ali',
+    'Pranav',
+    'Spencer',
+    'Ian',
+    'dnb',
+    'cba']
+  adjectives = ['magical',
+    'irresponsible',
+    'lame',
+    'a unicorn',  # okay, some of these aren't really adjectives
+    'dead',
+    'beautiful',
+    'Barbara Streissand']
+
+  # Pick a random element from each array
+  noun = nouns.sample
+  adjective = adjectives.sample
+
+  # Output
+  "#{noun} is #{adjective}"
 end
